ea03ad3f91000c7c725376fd41c4d6f3f40bd493
mukeshkr59 <mukeshkv20@iitk.ac.in>
2025-12-11 21:56:30 +0530

    login & signup done

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,67 +3,73 @@ import assets from "../assets/assets";
 import { useState } from "react";
 const LoginPage = () => {
     const [currState, setCurrState] = useState("Sign Up");
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [bio, setBio] = useState("");
+    const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault(); 
+        if(currState === "Sign Up" && !isDataSubmitted){
+            setIsDataSubmitted(true);
+        }
+    }
+
   return (
     <div
       className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       {/* ----- left */}
       <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
       {/* right */}
-      {currState === "Sign Up" ? 
-        <form action="">
-        <div className="bg-[#282142]/70 backdrop-blur-xl border border-gray-600 rounded-2xl p-8 flex flex-col gap-6 w-[300px] max-sm:w-[90vw]">
-          <h1 className="text-white text-2xl font-semibold text-center">
-            {currState}    
-            </h1>
-            <input
-                type="text"
-                placeholder="Full Name"
-                className="bg-transparent border border-gray-600 rounded-full py-2 px-4 text-white outline-none placeholder:text-gray-400 text-sm shadow-lg" required
-            />
-            <input
-                type="email"
-                placeholder="Email"
-                className="bg-transparent border border-gray-600 rounded-full py-2 px-4 text-white outline-none placeholder:text-gray-400 text-sm shadow-lg" required
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                className="bg-transparent border border-gray-600 rounded-full py-2 px-4 text-white outline-none placeholder:text-gray-400 text-sm" required
-            />
-            <button className="bg-linear-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
-                {currState}
-            </button>
-            <p className="text-xs text-gray-400 text-center">
-                Already have an account? <span onClick={()=>setCurrState("Login")} className="text-white cursor-pointer">Login</span>
-            </p>
-        </div>
-      </form> :
-        <form action="">
-        <div className="bg-[#282142]/70 backdrop-blur-xl border border-gray-600 rounded-2xl p-8 flex flex-col gap-6 w-[300px] max-sm:w-[90vw]">
-            <h1 className="text-white text-2xl font-semibold text-center">
-                {currState}
-            </h1>
-            <input
-                type="email"
-                placeholder="Email"
-                className="bg-transparent border border-gray-600 rounded-full py-2 px-4 text-white outline-none placeholder:text-gray-400 text-sm shadow-lg" required
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                className="bg-transparent border border-gray-600 rounded-full py-2 px-4 text-white outline-none placeholder:text-gray-400 text-sm" required
-            />
-            <button className="bg-linear-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
-                {currState}
-            </button>
-            <p className="text-xs text-gray-400 text-center">
-                Don't have an account? <span onClick={()=>setCurrState("Sign Up")} className="text-white cursor-pointer">Sign Up</span>
+      <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+        <h2 className='font-medium text-2xl flex justify-between items-center'>
+            {currState}
+            {isDataSubmitted && (
+               <img onClick={()=>setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
+            )}
+        </h2>
 
-            </p>
+        {currState === "Sign Up" && !isDataSubmitted && (
+            <input onChange={(e)=>setFullName(e.target.value)} value={fullName} type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder="Full Name" required/>
+        )}
+
+        {!isDataSubmitted && (
+            <>
+            <input onChange={(e)=>setEmail(e.target.value)} value={email}
+            type="email" placeholder="Email Address" required 
+            className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'/>
+            <input onChange={(e)=>setPassword(e.target.value)} value={password}
+            type="password" placeholder="Password" required 
+            className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'/>
+            </>
+        )}
+
+        {
+            currState==="Sign Up" && isDataSubmitted && (
+                <textarea onChange={(e)=>setBio(e.target.value)} value={bio} rows={4} placeholder="Provide a short Bio" 
+                className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'/>
+            )
+        }
+
+       <button type='submit' className='py-3 bg-linear-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
+          {currState === "Sign Up" ? "Create Account": "Login Now"}
+       </button>
+       <div className="flex items-center gap-2 text-sm text-gray-500">
+        <input type="checkbox" />
+        <p>Agree to the terms of use & privecy policy. </p>
+       </div>
+
+        <div className="flex flex-col gap-2">
+            {currState === "Sign Up" ? (
+                <p className="test-sm text-gray-600">Already have an account? <span onClick={()=>{setCurrState("Login"); setIsDataSubmitted(false);}} className='font-medium text-violet-500 cursor-pointer'>Login</span></p>
+            ) : (
+                <p className="test-sm text-gray-600">Don't have an account? <span onClick={()=>setCurrState("Sign Up")} className='font-medium text-violet-500 cursor-pointer'>Sign Up</span></p>
+            )  }
         </div>
+
+        {/* <input type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder="" required/> */}
       </form>
-      
-    }
     </div>
   );
 };
