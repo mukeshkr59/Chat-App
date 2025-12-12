3c3f8c4b63f6bcb8700376ddb2f91a29d90249ca
mukeshkr59 <mukeshkv20@iitk.ac.in>
2025-12-12 11:49:04 +0530

    Frontend Done

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -19,21 +19,21 @@ const ProfilePage = () => {
   return (
     <div className='min-h-screen flex items-center justify-center bg-cover bg-no-repeat'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
-        <form onSubmit={onSubmitHandler} className='flex flex-col gap-5 p-10 flex-1' action="">
+        <form onSubmit={onSubmitHandler} className='flex flex-col gap-5 p-10 flex-1' >
           <h3 className='text-lg'>Profile details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
             <input onChange={(e)=>setSelectedImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
-            <img src={selectedImage?URL.createObjectURL(selectedImage): assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImage && 'rounded-full'}`} />
+            <img src={selectedImage?URL.createObjectURL(selectedImage): assets.avatar_icon} className={`w-12 h-12 ${selectedImage && 'rounded-full'}`} />
             Upload profile image
           </label>
           <input onChange={(e)=>setName(e.target.value)} value={name} type="text" required placeholder='Your Name' 
           className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500 '/>
-          <textarea onChange={(e)=>setBio(e.target.value)} value={bio} placeholder='Write Profile Bio' required name="" id="" rows={4}
+          <textarea onChange={(e)=>setBio(e.target.value)} value={bio} placeholder='Write Profile Bio' required  rows={4}
           className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' ></textarea>
           <button type='submit' 
           className='bg-linear-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'> Save </button>
         </form>
-        <img src={assets.logo_icon} alt=""
+        <img src={assets.logo_icon} 
         className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' />
       </div>
     </div>
