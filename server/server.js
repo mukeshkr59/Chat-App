a86e2cc8655a60d77f6dde9b8c5285cb38fb7612
Mukesh Kumar Vishwakarma <91455525+mukeshkr59@users.noreply.github.com>
2025-12-20 21:58:35 +0530

    Conditionally start server based on NODE_ENV

--- server/server.js
+++ server/server.js
@@ -52,8 +52,12 @@ app.use("/api/messages", messageRouter); // Assuming messageRouter is defined el
 // connect to MongoDB
 await connectDB();
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if(process.env.NODE_ENV !== "production"){
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default server;
 
         
