094ebb4be2eb811555eff6293bb081c9cf483a18
Mukesh Kumar Vishwakarma <91455525+mukeshkr59@users.noreply.github.com>
2025-12-20 22:07:21 +0530

    Uncomment App component and fix background image path
    Restored commented-out code and updated background image path.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,3 @@
-// import { Route, Routes, Navigate } from "react-router-dom";
-// import HomePage from "./pages/HomePage.jsx";
-// import LoginPage from "./pages/LoginPage.jsx";
-// import ProfilePage from "./pages/ProfilePage.jsx";
-// import { Toaster } from "react-hot-toast";
-// import { useContext } from "react";
-// import { AuthContext } from "../context/AuthContext";
-
-// const App = () => {
-//   const { authUser } = useContext(AuthContext);
-
-
-//   return (
-//     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
-//       <Toaster />
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={authUser ? <HomePage /> : <Navigate to="/login" />}
-//         />
-//         <Route
-//           path="/login"
-//           element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-//         />
-//         <Route
-//           path="/profile"
-//           element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-//         />
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -46,7 +10,7 @@ const App = () => {
   const { authUser } = useContext(AuthContext);
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
         <Route
