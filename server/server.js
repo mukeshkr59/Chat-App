1d3a26a995a051f0ebebe45bacf5ee7310756328
Mukesh Kumar Vishwakarma <91455525+mukeshkr59@users.noreply.github.com>
2026-02-27 15:45:28 +0530

    Fix formatting issue in allowedOrigins array

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   process.env.FRONTEND_URL, 
-
+];
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
